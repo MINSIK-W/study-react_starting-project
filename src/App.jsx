585e95f527a54d69cf5572a75cec32cc7573e33b
minsik90 <minsik90@icloud.com>
2024-07-18 16:06:48 +0900

    props (attribute) Alternative Grammar 1-2 [Recommended]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import reactImg from './assets/react-core-concepts.png'
-import componentsImg from './assets/components.png'
 import { CORE_CONCEPTS } from './data.js'
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
@@ -41,26 +40,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
