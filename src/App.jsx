90a0c3c8e88ef31f901cb06394016cfa3570f539
minsik90 <minsik90@icloud.com>
2024-07-19 16:25:11 +0900

    Separating Components into Features and State

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,16 @@
-import { useState } from 'react'
-
-import { CORE_CONCEPTS, EXAMPLES } from './data.js'
-
 import Header from './components/Header/Header'
-import CoreConcept from './components/CoreConcept'
-import TapButton from './components/TapButton'
+import Examples from './components/Examples'
+import CoreConcepts from './components/CoreConcepts'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState()
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'JSX', Props, State
-    setSelectedTopic(selectedButton)
-    console.log(setSelectedTopic)
-  }
-
   console.log('APP COMPONENT EXECUTING')
 
-  let topContent = <p>Please select a topic.</p>
-  if (selectedTopic) {
-    topContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    )
-  }
   return (
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map(conceptItem => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TapButton
-              isSelected={selectedTopic === 'components'}
-              onSelect={() => handleSelect('components')}
-            >
-              Components
-            </TapButton>
-            <TapButton
-              isSelected={selectedTopic === 'jsx'}
-              onSelect={() => handleSelect('jsx')}
-            >
-              JSX
-            </TapButton>
-            <TapButton
-              isSelected={selectedTopic === 'props'}
-              onSelect={() => handleSelect('props')}
-            >
-              Props
-            </TapButton>
-            <TapButton
-              isSelected={selectedTopic === 'state'}
-              onSelect={() => handleSelect('state')}
-            >
-              State
-            </TapButton>
-          </menu>
-        </section>
-        {topContent}
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   )
---
+++ src/components/Examples.jsx
@@ -0,0 +1,58 @@
+import TapButton from './TapButton'
+import { useState } from 'react'
+import { EXAMPLES } from '../data'
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState()
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'JSX', Props, State
+    setSelectedTopic(selectedButton)
+    console.log(setSelectedTopic)
+  }
+
+  let topContent = <p>Please select a topic.</p>
+  if (selectedTopic) {
+    topContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    )
+  }
+
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TapButton
+          isSelected={selectedTopic === 'components'}
+          onSelect={() => handleSelect('components')}
+        >
+          Components
+        </TapButton>
+        <TapButton
+          isSelected={selectedTopic === 'jsx'}
+          onSelect={() => handleSelect('jsx')}
+        >
+          JSX
+        </TapButton>
+        <TapButton
+          isSelected={selectedTopic === 'props'}
+          onSelect={() => handleSelect('props')}
+        >
+          Props
+        </TapButton>
+        <TapButton
+          isSelected={selectedTopic === 'state'}
+          onSelect={() => handleSelect('state')}
+        >
+          State
+        </TapButton>
+      </menu>
+      {topContent}
+    </section>
+  )
+}
