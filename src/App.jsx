cb75225309c98df19d228e7f4bcb8030017b1071
minsik90 <minsik90@icloud.com>
2024-07-19 14:01:16 +0900

    Conditional Content Rendering 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CoreConcept from './components/CoreConcept'
 import TapButton from './components/TapButton'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState()
 
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'JSX', Props, State
@@ -44,13 +44,17 @@ function App() {
             <TapButton onSelect={() => handleSelect('props')}>Props</TapButton>
             <TapButton onSelect={() => handleSelect('state')}>State</TapButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </>
